3150354ac374d431067475c70286cfc2c79f2ac5
jun-uen0 <hello@junueno.dev>
2022-07-23 08:53:44 +0900

    added some exlaination

--- py/same-tree.py
+++ py/same-tree.py
@@ -56,12 +56,14 @@ class Solution:
 
 ###############################################################################
 # Other's Solution:
-# Definition for a binary tree node.
+# Recursive
 class Solution:
   def isSameTree(self, p, q):
     try:
-      if (p==None)&(q==None): return True
-      elif (p.val==q.val)&(self.isSameTree(p.left,q.left))&(self.isSameTree(p.right,q.right)): return True
+      if (p==None)&(q==None):
+        return True
+      elif (p.val==q.val)&(self.isSameTree(p.left,q.left))&(self.isSameTree(p.right,q.right)):
+        return True
       else:return False
     except:
       return False
