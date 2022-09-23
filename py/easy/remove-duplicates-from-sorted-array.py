cb73aeb3b8383081611933b218e66942bddd60ed
jun-uen0 <hello@junueno.dev>
2022-09-23 17:29:06 +0900

    mv from recap

--- py/easy/remove-duplicates-from-sorted-array.py
+++ py/easy/remove-duplicates-from-sorted-array.py
@@ -13,13 +13,13 @@
 # Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 
 # Constraints:
-
 # 1 <= nums.length <= 3 * 104
 # -100 <= nums[i] <= 100
 # nums is sorted in non-decreasing order.
 
 from typing import List # Need this for the type annotation
 nums = [1,1,1,2,2,3,3,4]
+
 def remove_duplicates(nums: List[int]) -> int:
   d = 0
   for i in range(1,len(nums)):
@@ -27,6 +27,5 @@ def remove_duplicates(nums: List[int]) -> int:
       d+=1
     else:
       nums[i-d] = nums[i]
-  print(nums)
   return len(nums) - d
 print(remove_duplicates(nums)) # 4
