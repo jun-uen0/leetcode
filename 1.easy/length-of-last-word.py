4a848eaf04013ae98a6940a662588ff6059912b5
Jun Ueno <hello@junueno.dev>
2023-07-29 11:34:14 +0900

    ch dir stracture

---
+++ 1.easy/length-of-last-word.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/length-of-last-word
+
+# Problem:
+# Given a string s consisting of words and spaces,
+# return the length of the last word in the string.
+# A word is a maximal substring consisting of non-space characters only.
+
+# Example 1:
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+
+# Example 2:
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+
+# My solution:
+# 1. Split the string by space
+# 2. Add every splited word in a list
+# 3. Reture the number of last word
+
+s = "   fly me   to   the moon  "
+print(len(s.split()[-1]))
