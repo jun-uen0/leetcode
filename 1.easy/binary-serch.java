c0b1ce3e6ea626536e814404788d944090177fb5
jun-uen0 <hello@junueno.dev>
2025-01-09 21:43:18 +0900

    add: binary-serch with Java

---
+++ 1.easy/binary-serch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1; // 5
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if(nums[middle] == target) {
+                return middle;
+            } else if (target > nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
