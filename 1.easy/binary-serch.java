6eaa4e269170c3b6b0437564f22cb5fedcf6b251
jun-uen0 <hello@junueno.dev>
2025-01-26 11:22:43 +0900

    update: binary search

--- 1.easy/binary-serch.java
+++ 1.easy/binary-serch.java
@@ -1,16 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
+    
         int left = 0;
-        int right = nums.length - 1; // 5
+        int right = nums.length - 1;
 
         while (left <= right) {
+            
             int middle = (left + right) / 2;
 
-            if(nums[middle] == target) {
+            if (nums[middle] == target) {
                 return middle;
-            } else if (target > nums[middle]) {
+            }
+
+            if (nums[middle] < target) {
                 left = middle + 1;
-            } else {
+            }
+
+            if (nums[middle] > target) {
                 right = middle - 1;
             }
         }
@@ -18,8 +24,3 @@ class Solution {
         return -1;
     }
 }
-
-for (int i = 0; i < 配列の長さ -1; i++) {
-    // iが5783だったらtrue、もしくはインデックス番号を返す
-}
-
